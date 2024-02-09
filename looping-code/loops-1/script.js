3cb0c23e110ce3637b4b828ece0a478b220b27bf
Adhitya Aditama Nugraha <aaditamanugraha@Adhityas-MacBook-Air.local>
2024-02-09 21:46:03 +0800

    Solve loops 1

--- looping-code/loops-1/script.js
+++ looping-code/loops-1/script.js
@@ -2,7 +2,17 @@ const myArray = ["tomatoes", "chick peas", "onions", "rice", "black beans"];
 const list = document.createElement("ul");
 
 // Add your code here
+const startBtn = document.querySelector("button");
+const preview = document.querySelector(".preview");
 
+startBtn.addEventListener("click", () => {
+  for (let i = 0; i < myArray.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = myArray[i];
+    list.appendChild(listItem);
+    preview.appendChild(list);
+  }
+});
 // Don't edit the code below here!
 
 const section = document.querySelector("section");
